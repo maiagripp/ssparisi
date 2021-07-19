0987199931ef84d6bee581ddf1bc55de77d0b2c9
maiagripp <maiaandradec@gmail.com>
2021-07-19 13:59:57 -0300

    first commit

--- src/Api/server.js
+++ src/Api/server.js
@@ -1,28 +1,3 @@
-// const express = require('express');
-// const fileupload = require('express-fileupload');
-// const cors = require('cors');
-
-// const app = express();
-
-// app.use(cors());
-// app.use(fileupload());
-// app.use(express.static('files'));
-
-// app.post('/upload', (req, res) => {
-//   const newpath = __dirname + '/files/';
-//   const file = req.files.file;
-
-//   file.mv(`${newpath}`, (err) => {
-//     if (err) {
-//       res.status(500).send({ message: 'File upload failed', code: 200 });
-//     }
-//     res.status(200).send({ message: 'File Uploaded', code: 200 });
-//   });
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server running successfully on 3000');
-// });
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
@@ -54,24 +29,25 @@ app.post('/upload', async (req, res) => {
         'https://date.nager.at/api/v3/publicholidays/2021/BR'
       );
 
-      const feriados = responseApi.data;
+      const holidays = responseApi.data;
       let files = req.files.files;
 
-      const eventosEmFeriado = [];
+      const events = [];
+      // const eventosEmFeriado = [];
 
       const lines = [];
 
       lines.forEach((line) => {
-        const existeFeriado = feriados.find(
-          (feriado) => feriado.date === line.data
+        const ifHoliday = holidays.find(
+          (holiday) => holiday.date === line.data
         );
 
-        if (existeFeriado) {
-          eventosEmFeriado.push(line);
+        if (ifHoliday) {
+          events.push(line);
         }
       });
 
-      res.send(eventosEmFeriado);
+      res.send(events);
     }
   } catch (err) {
     res.status(500).send(err);
